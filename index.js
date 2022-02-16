4a3ed62db3681a8568fa392ef8fed453ee30b331
stanleyos <stanislaw.sarka@gmail.com>
2022-02-16 17:40:31 +0100

    Some changes- second commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // console.log("Hello FizzBazz!");
 for (let i = 1; i < 100; i++) {
   console.log((i % 3 == 0 ? "Fizz" : "") + (i % 5 == 0 ? "Buzz" : "") || i);
+
+  // comment for second commit
 }
